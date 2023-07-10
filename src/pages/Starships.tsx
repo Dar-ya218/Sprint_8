7a160360ccf3061dbfc07bee9188488a7cb9f230
Dar-ya218 <dasharoura13465@gmail.com>
2023-07-10 13:08:48 +0200

    ejercicio_1

--- src/pages/Starships.tsx
+++ src/pages/Starships.tsx
@@ -1,11 +1,44 @@
+import { useEffect, useState } from "react";
 import { Navbar } from "../components/Navbar";
+import axios from "axios";
+
+interface Starship{
+    name: string
+    model: string
+    index: number
+}
 
 export function Starships() {
+const [starships, setStarships]= useState<Starship[]>([])
+
+const callAPI = async()=>{
+const res = await axios.get('https://swapi.dev/api/starships/')
+console.log('llamada API', res.data)
+return res.data
+}
+
+const getStarships = async()=>{
+    const data = await callAPI()
+    setStarships(data.results as Starship[])
+}
+
+useEffect(()=>{
+    getStarships()
+}, [])
+console.log('starships', starships)
+
     return (
         <>
             <Navbar />
-            <div>
-                <h1>Starships</h1>
+            <div className="starshipsPageDiv">
+                {starships.map((starship, index)=>{
+                    return(
+                        <div key={index} className="starshipInfoDiv">
+                            <div className="starshipName">{starship.name.toUpperCase()}</div>
+                            <div className="starshipModel">{starship.model}</div>
+                        </div>
+                    )
+                })}
             </div>
         </>
     );
